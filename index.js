5604f9363c144937d7eb9d3a5a5f1c01121c46f9
RicardoDomi <ricardo.dominguez4666@alumnos.udg.mx>
2025-09-28 02:38:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const authRoutes = require('./src/routes/auth');
 const journeysRoutes = require('./src/routes/journey');
 
 const helmet = require('helmet');
-const authRoutes = require('./service/api/auth');
+const path = require('path')
+
 
 
 
@@ -15,7 +16,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(helmet());
+app.use(helmet({contentSecurityPolicy: false, crossOriginResourcePolicy: {policy: 'cross-origin'},}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/auth', authRoutes);
 
 app.use('/routes', journeysRoutes);
@@ -25,6 +27,8 @@ app.use('/routes', journeysRoutes);
 app.get('/', (req, res) => {
   res.send('API RESTful Movilidad funcionando');
 });
+
+app.get('/login', (_req, res)=> res.sendFile(path.join(__dirname, 'public', 'login.html')));
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
