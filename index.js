bbafdf5df3275be0046c199515a2ada1d5966b5e
Kraata-code <jorgekastolo@gmail.com>
2025-10-04 11:19:00 -0600

    Logger pino

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const express = require('express');
-const dotenv = require('dotenv');
-
-const authRoutes = require('./src/routes/auth');
-const journeysRoutes = require('./src/routes/journey');
-
-const helmet = require('helmet');
-const authRoutes = require('./service/api/auth');
-
-
-
+const express = require("express");
+const dotenv = require("dotenv");
+const journeysRoutes = require("./src/routes/journey");
+const helmet = require("helmet");
+const authRoutes = require("./src/routes/auth");
+const pino = require("pino");
+const pinoHttp = require('pino-http');
 dotenv.config();
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
+});
+
+logger.info("hello world");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(pinoHttp());
 app.use(express.json());
 app.use(helmet());
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
-app.use('/routes', journeysRoutes);
+app.use("/routes", journeysRoutes);
 
- 
-
-app.get('/', (req, res) => {
-  res.send('API RESTful Movilidad funcionando');
+app.get("/", (req, res) => {
+  res.send("API RESTful Movilidad funcionando");
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+  logger.info(`Servidor escuchando en el puerto ${PORT}`);
 });
