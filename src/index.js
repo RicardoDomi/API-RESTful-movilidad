e112222dc0e262fd95094656a22f13eeccf14e7c
DanielEspinoza77 <espinozadani96@gmail.com>
2025-10-03 20:59:33 -0600

    logger morgan , and also nodemon

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
-const authRoutes = require('./service/api/auth');
-
-
+const morgan = require('morgan');
+//const authRoutes = require('./service/api/auth');
+    
+ 
 dotenv.config();
-
+ 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+   
 app.use(express.json());
 app.use(helmet());
-app.use('/auth', authRoutes);
- 
+//app.use('/auth', authRoutes);
+  
+app.use(morgan('combined'));
+
 app.get('/', (req, res) => {
-  res.send('API RESTful Movilidad funcionando');
+  res.send('API RESTful Movilidad funcionando');   
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+});   
